ab670322747d6579716dd4d41f28ecc8e2e6682d
Tony <tony09thomas@gmail.com>
2017-08-15 16:41:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article-one', function(req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  
 });
 app.get('/article-two', function(req, res){
-  res.send('article-two requested and will be served');  
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function(req, res){
-  res.send('article-three requested and will be served');  
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));  
 });
 
 app.get('/ui/style.css', function (req, res) {
