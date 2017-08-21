456d2a066edd722798be9dcea98947d628c2b55d
Tony <tony09thomas@gmail.com>
2017-08-21 23:43:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+//move drago
+var img=document.getElementById("drago");
+var marginleft=0;
+    function moveright(){
+        marginleft=marginleft+10;
+        img.style.marginleft=marginleft+'px';
+    }
+    img.onclick=function(){
+  var intervel=SetIntervel(moveright,100);  
+};
