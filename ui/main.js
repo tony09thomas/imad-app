12b63273ae7c57ce506440b09c1f80929665d459
Tony <tony09thomas@gmail.com>
2017-08-24 00:18:51 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,24 @@
+//counter code
+var button=document.getElementById('counter');
+
+button.onClock=function(){
+    
+    //create a request object
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET', 'http://tony09thomas.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
