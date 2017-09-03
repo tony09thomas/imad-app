246ad1fc76c1f71c7c20a91950ac95b035d821cb
Tony <tony09thomas@gmail.com>
2017-09-03 14:08:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,10 +65,21 @@ app.get('/counter', function(req, res){
 });
 
 
-app.get('/articleName', function(req, res){
-    var articleName = req.params.articleName;
+app.get('/articles//articleName', function(req, res){
     
-  res.send(createTemplate(articles[articleName]));  
+    
+    pool.query("SELECT * FROM article WHERE title="+req.params.articleName, function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.length ===0){
+               res.status(404).send('Article not found');
+           }else{
+               var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+           }
+       } 
+    });
 });
 
 
