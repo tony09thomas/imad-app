25ef110ad0f9574e220c0840429bba289f873c0d
Tony <tony09thomas@gmail.com>
2017-09-03 14:12:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/counter', function(req, res){
 app.get('/articles//articleName', function(req, res){
     
     
-    pool.query("SELECT * FROM article WHERE title='" + req.params.articleName + "'", function(err,result){
+    pool.query("SELECT * FROM article WHERE title=' + req.params.articleName + '", function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }else{
