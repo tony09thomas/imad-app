8d1bb90be22e4186e6aa1a6e1c13039d3764d027
Tony <tony09thomas@gmail.com>
2017-09-08 00:12:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ function hash(input,salt){
 }
 
 
-app.get('/hash/input',function(req, res){
+app.get('/hash:input',function(req, res){
     var hashedString=hash(req.params.input, 'this is some random string');
     res.send(hashedString);
     
